61524a684bade182dd9ea1c5c979448d247bf8b5
denis <9172885049@mail.ru>
2019-10-21 22:54:58 +0300

    vers1

---
+++ lesson-Millioner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson-Millioner
+//
+//  Created by admin on 21.10.2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
